8a51ca4290fb19db43fced7d5a863de8dcc9ccdf
DragonDC <damianoss.c@gmail.com>
2015-04-13 12:03:52 +0200

    changes in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Damian Super User",
+             email: "damian@user.pl",
+             password:              "admin",
+             password_confirmation: "admin",
              admin:     true)
 
 99.times do |n|
